d8cb7e4fa36693757f8bc7816b8d77e333348205
calfonsoQA <75623928+calfonsoQA@users.noreply.github.com>
2021-01-29 12:16:46 +0000

    some changes to carcontroller js and html file

--- CarController.js
+++ CarController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const peeps = document.querySelector("#peeps"); 
-const carName = document.querySelector("#carName"); 
+const carBrand = document.querySelector("#carBrand"); 
 const make = document.querySelector("#make"); 
 const alert = document.querySelector("#onsuccess");
 const modal = document.querySelector("#firstModal");
@@ -15,7 +15,7 @@ const printNameToScreen = (carName) => {
 }
 
 const retrieveData = () => {
-    fetch("htps://reqres.in/api/utsers")
+    fetch("http://localhost:9092/swagger-ui/index.html")
     .then((response) => {
         // check that the response is OK (i.e. 200)
         if(response.status !== 200){
@@ -37,3 +37,35 @@ const retrieveData = () => {
         console.error(err);
     })
 }
+
+const createCar = () => {
+    const carValue = carBrand.value; 
+    const makeValue = make.value;
+
+    let data = {
+        brand: carValue, 
+        make: makeValue
+    }
+
+    fetch("http://localhost:9092/swagger-ui/index.html",{
+        method: "POST", 
+        body: JSON.stringify(data),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(info => {
+        console.log(info);
+        alert.setAttribute("class", "alert alert-success"); 
+        alert.innerHTML = "User has been successfully created!"; 
+        setTimeout( () => {
+           alert.removeAttribute("class"); 
+           alert.innerHTML = ""; 
+        },2000);
+    })
+    .catch(err => console.error(`Stopppppp! ${err}`));
+}
+
+
+retrieveData(); 
